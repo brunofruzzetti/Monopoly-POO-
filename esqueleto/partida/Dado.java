5ad512e9e137d8c7f4d4ea26c15afbd6549d756d
robercastro123 <robertomartinezcastro07@gmail.com>
2026-09-22 12:40:03 +0200

    Update Dado.java
    hacerTirada funcional

--- esqueleto/partida/Dado.java
+++ esqueleto/partida/Dado.java
@@ -1,5 +1,6 @@
 package partida;
 
+import java.util.Random;
 
 public class Dado {
     //El dado solo tiene un atributo en nuestro caso: su valor.
@@ -7,7 +8,13 @@ public class Dado {
 
     //Metodo para simular lanzamiento de un dado: devolverá un valor aleatorio entre 1 y 6.
     public int hacerTirada() {
-        return 6;
-    }
 
+        Random random = new Random();
+        
+        // la funcion random.nextInt genera un numero entre 0 y n (excluyendo n)
+        int dado = random.nextInt(6) + 1;
+        System.out.println("Salió un: " + dado);
+        
+        return dado;
+    }
 }
